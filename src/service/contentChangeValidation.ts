0fce7d5647928abbec7fce0134c392ebff88b05f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-04-14 11:27:16 -0300

    refactor(src/service/contentChangeValidation.ts): refactor structure with base at auth strategy.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/service/contentChangeValidation.ts
+++ src/service/contentChangeValidation.ts
@@ -1,10 +1,6 @@
-import { getInput, info, setOutput } from '@actions/core'
-import { GitHubAppRepository } from '@repositories/GitHubAppRepository'
-import { GitHubPATRepository } from '@repositories/GitHubPATRepository'
-import { compareDate } from 'utils'
-
-const gitHubAppRepository = new GitHubAppRepository()
-const gitHubPATRepository = new GitHubPATRepository()
+import { getInput } from '@actions/core'
+import { gitHubAppService } from './gitHubAppService'
+import { gitHubPATService } from './gitHubPATService'
 
 const authType = getInput('authStrategy')
 
@@ -15,88 +11,18 @@ export async function contentChangeValidation(
   repoOwner: string,
 ): Promise<any> {
   if (authType === 'app') {
-    const lastModifiedDefaultBranch =
-      await gitHubAppRepository.getLastModifiedDate(
-        directoryOrFile,
-        repoName,
-        repoOwner,
-      )
-
-    const branchBasePR = await gitHubAppRepository.getBranchBase(
+    await gitHubAppService(
+      directoryOrFile,
       pullRequestNumber,
       repoName,
       repoOwner,
     )
-
-    const lastUpdateBranchBasePR =
-      await gitHubAppRepository.getLastCommitBranchBase(
-        branchBasePR,
-        directoryOrFile,
-        repoName,
-        repoOwner,
-      )
-
-    const validateModified = compareDate(
-      lastModifiedDefaultBranch,
-      lastUpdateBranchBasePR,
-    )
-
-    if (validateModified == false) {
-      await gitHubAppRepository.createCommentAtPR(
-        `Changes were made to "${directoryOrFile}". These modifications are not allowed according to the organization/repository administrators.`,
-        pullRequestNumber,
-        repoName,
-        repoOwner,
-      )
-      return false
-    }
-
-    info(
-      `This implementation did not make changes to ${directoryOrFile}, as per the standards defined by the organization/administrators of the repository.`,
-    )
-    setOutput('Validation Status', 'Approved')
-    return true
   } else if (authType == 'pat') {
-    const lastModifiedDefaultBranch =
-      await gitHubPATRepository.getLastModifiedDate(
-        directoryOrFile,
-        repoName,
-        repoOwner,
-      )
-
-    const branchBasePR = await gitHubPATRepository.getBranchBase(
+    await gitHubPATService(
+      directoryOrFile,
       pullRequestNumber,
       repoName,
       repoOwner,
     )
-
-    const lastUpdateBranchBasePR =
-      await gitHubPATRepository.getLastCommitBranchBase(
-        branchBasePR,
-        directoryOrFile,
-        repoName,
-        repoOwner,
-      )
-
-    const validateModified = compareDate(
-      lastModifiedDefaultBranch,
-      lastUpdateBranchBasePR,
-    )
-
-    if (validateModified == false) {
-      await gitHubPATRepository.createCommentAtPR(
-        `Changes were made to "${directoryOrFile}". These modifications are not allowed according to the organization/repository administrators.`,
-        pullRequestNumber,
-        repoName,
-        repoOwner,
-      )
-      return false
-    }
-
-    info(
-      `This implementation did not make changes to ${directoryOrFile}, as per the standards defined by the organization/administrators of the repository.`,
-    )
-    setOutput('Validation Status', 'Approved')
-    return true
   }
 }
